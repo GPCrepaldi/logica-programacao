3b1b8eac4920343b2b5ea644d9e266870af646dd
GPCrepaldi <86982901+GPCrepaldi@users.noreply.github.com>
2023-07-20 11:37:54 -0300

    novos exercícios

---
+++ exercicios-solo/while.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+void main ()
+{
+    int x, soma;
+    soma = 0;
+
+    printf("Digite o primeiro numero: ");
+    scanf("%d", &x);
+
+    while (x != 0)
+    {
+        soma = soma + x;
+        printf("Digite outro numero: ");
+        scanf("%d", &x);
+    }
+
+    printf("Soma = %d\n", soma);
+    
+    return 0;
+        
+}
