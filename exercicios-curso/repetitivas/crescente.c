21acf3022194c3729d305adc9fc42cbf73c9c709
GPCrepaldi <86982901+GPCrepaldi@users.noreply.github.com>
2023-07-25 15:30:55 -0300

    exercicio crescente

---
+++ exercicios-curso/repetitivas/crescente.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main () {
+
+    int x, y;
+
+    printf("Digite dois numeros:\n");
+    scanf("%d %d", &x, &y);
+
+    while ( x != y) {
+        if (x > y) {
+            printf("\nDECRESCENTE!!!\n");
+        } 
+        else {
+            printf("\nCRESCENTE!!!\n");
+        }
+
+        printf("Digite outros dois numeros: ");
+        scanf("%d %d", &x, &y);
+
+    }
+
+    return 0;
+}
