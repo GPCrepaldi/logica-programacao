5832b8047954c3f0ffed206ccf29c57ca72da11b
Gabriel Pedreiro Crepaldi <86982901+GPCrepaldi@users.noreply.github.com>
2023-08-24 22:08:47 -0300

    exercicio experiencia começado

---
+++ exercicios-curso/repetitivas/experiencias.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main () {
+
+    int testes, cobaia, total, tc, tr, ts;
+    double pc, pr, ps;
+    char tipo;
+
+    printf("Quantos casos de teste serao digitados? ");
+    scanf("&d", &testes);
+
+    for (i = 0; i < count; i++)
+    {
+        /* code */
+    }
+    
+}
