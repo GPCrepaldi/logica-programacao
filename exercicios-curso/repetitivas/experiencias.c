b5bf605464a7b4408eea3f0f24504c1e498a639c
Gabriel Pedreiro Crepaldi <86982901+GPCrepaldi@users.noreply.github.com>
2023-08-26 13:33:45 -0300

    exercicio experiencia concluido

--- exercicios-curso/repetitivas/experiencias.c
+++ exercicios-curso/repetitivas/experiencias.c
@@ -2,39 +2,42 @@
 
 int main () {
 
-    int testes, cobaia, cobaiaT, rato = 0, sapo = 0, coelho = 0, total, tc, tr, ts;
+    int testes, cobaia, cobaiaT, ratos = 0, sapos = 0, coelhos = 0;
     double pc, pr, ps;
     char tipo;
 
     printf("Quantos casos de teste serao digitados? ");
-    scanf("&d", &testes);
+    scanf("%d", &testes);
 
-    for (int i = 1; i <= testes; i++) {
-        printf("Quantidades de cobaia: ");
-        scanf("%d", &cobaia);
+        for (int i = 0; i < testes; i++) {
+            printf("Quantidades de cobaia: ");
+            scanf("%d", &cobaia);
 
-        printf("Tipo de cobaia: ");
-        scanf("%c", &tipo);
+            printf("Tipo de cobaia: ");
+            scanf(" %c", &tipo);
 
-        if ( tipo == 'r' || tipo == 'R') {
-            rato = rato + cobaia;
-        } if else ( tipo == 's' || tipo == 'S' ) {
-            sapo = sapo + cobaia;
-        } if else ( tipo == 'c' || tipo == 'C' ) {
-            coelho = coelho + cobaia;
+                if ( tipo == 'r' || tipo == 'R') {
+                    ratos = ratos + cobaia;
+                } else if ( tipo == 's' || tipo == 'S' ) {
+                    sapos = sapos + cobaia;
+                } else if ( tipo == 'c' || tipo == 'C' ) {
+                    coelhos = coelhos + cobaia;
+                }
         }
 
-        cobaiaT =+ cobaia;
-    }
-
-    percentual = ;
+    cobaiaT = ratos + coelhos + sapos;
+    pc = ((double)coelhos / cobaiaT) * 100;
+    pr = ((double)ratos / cobaiaT) * 100;
+    ps = ((double)sapos / cobaiaT) * 100;
 
     printf("\nRELATORIO FINAL :\n");
-    printf("Total: \n");
-    printf("Total de coelhos: \n");
-    printf("Total de ratos: \n");
-    printf("Total de sapos: \n");
-    printf("Percentual de coelhos: \n");
-    printf("Percentual de ratos: \n");
-    printf("Percentual de sapos: \n");
+    printf("Total: %d\n", cobaiaT);
+    printf("Total de coelhos: %d\n", coelhos);
+    printf("Total de ratos: %d\n", ratos);
+    printf("Total de sapos: %d\n", sapos);
+    printf("Percentual de coelhos: %.2lf\n", pc);
+    printf("Percentual de ratos: %.2lf\n", pr);
+    printf("Percentual de sapos: %.2lf\n", ps);
+
+return 0;
 }
