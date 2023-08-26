10e77322e77420a74ee0fa044e0dcedcbe1b73b3
Gabriel Pedreiro Crepaldi <86982901+GPCrepaldi@users.noreply.github.com>
2023-08-25 21:43:07 -0300

    experiencia 3

--- exercicios-curso/repetitivas/experiencias.c
+++ exercicios-curso/repetitivas/experiencias.c
@@ -2,24 +2,39 @@
 
 int main () {
 
-    int testes, cobaia, total, tc, tr, ts;
+    int testes, cobaia, cobaiaT, rato = 0, sapo = 0, coelho = 0, total, tc, tr, ts;
     double pc, pr, ps;
     char tipo;
 
     printf("Quantos casos de teste serao digitados? ");
     scanf("&d", &testes);
 
-    for (i = 0; i <= testes; i++) {
+    for (int i = 1; i <= testes; i++) {
         printf("Quantidades de cobaia: ");
         scanf("%d", &cobaia);
 
         printf("Tipo de cobaia: ");
         scanf("%c", &tipo);
 
-        if (tipo == 'c' || tipo == 'C') {
-            
+        if ( tipo == 'r' || tipo == 'R') {
+            rato = rato + cobaia;
+        } if else ( tipo == 's' || tipo == 'S' ) {
+            sapo = sapo + cobaia;
+        } if else ( tipo == 'c' || tipo == 'C' ) {
+            coelho = coelho + cobaia;
         }
-        
+
+        cobaiaT =+ cobaia;
     }
-    
+
+    percentual = ;
+
+    printf("\nRELATORIO FINAL :\n");
+    printf("Total: \n");
+    printf("Total de coelhos: \n");
+    printf("Total de ratos: \n");
+    printf("Total de sapos: \n");
+    printf("Percentual de coelhos: \n");
+    printf("Percentual de ratos: \n");
+    printf("Percentual de sapos: \n");
 }
