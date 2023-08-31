0ea426af8ac94cc2dea60a6c1b41d9316faa175b
Gabriel Pedreiro Crepaldi <86982901+GPCrepaldi@users.noreply.github.com>
2023-08-31 11:34:47 -0300

    realocação

--- C/exercicios-curso/condicional/aumento.c
+++ C/exercicios-curso/condicional/aumento.c
@@ -8,38 +8,34 @@ int main () {
     printf("Digite o salario atual: ");
     scanf("%lf", &salario);
 
-    if (salario <= 1000)
-    {
-        novo = salario * 1.2;
-        aumento = novo - salario;
-        printf("\nNovo salario sera de: R$ %.2lf\n", novo);
-        printf("Aumento: %.2lf\n", aumento);
-        printf("Aumento de: 20%%");
-    }
-    else if (salario > 1000 && salario <= 3000)
-    {
-        novo = salario * 1.15;
-        aumento = novo - salario;
-        printf("\nNovo salario sera de: R$ %.2lf\n", novo);
-        printf("Aumento: %.2lf\n", aumento);
-        printf("Aumento de: 15%%");
-    }
-    else if (salario > 3000 && salario <= 8000)
-    {
-        novo = salario * 1.1;
-        aumento = novo - salario;
-        printf("\nNovo salario sera de: R$ %.2lf\n", novo);
-        printf("Aumento: %.2lf\n", aumento);
-        printf("Aumento de: 10%%");
-    }
-    else 
-    {
-        novo = salario * 1.05;
-        aumento = novo - salario;
-        printf("\nNovo salario sera de: R$ %.2lf\n", novo);
-        printf("Aumento: %.2lf\n", aumento);
-        printf("Aumento de: 5%%");
-    }   
+        if (salario <= 1000) {
+            novo = salario * 1.2;
+            aumento = novo - salario;
+            printf("\nNovo salario sera de: R$ %.2lf\n", novo);
+            printf("Aumento: %.2lf\n", aumento);
+            printf("Aumento de: 20%%");
+        }
+        else if (salario > 1000 && salario <= 3000) {
+            novo = salario * 1.15;
+            aumento = novo - salario;
+            printf("\nNovo salario sera de: R$ %.2lf\n", novo);
+            printf("Aumento: %.2lf\n", aumento);
+            printf("Aumento de: 15%%");
+        }
+        else if (salario > 3000 && salario <= 8000) {
+            novo = salario * 1.1;
+            aumento = novo - salario;
+            printf("\nNovo salario sera de: R$ %.2lf\n", novo);
+            printf("Aumento: %.2lf\n", aumento);
+            printf("Aumento de: 10%%");
+        }
+        else {
+            novo = salario * 1.05;
+            aumento = novo - salario;
+            printf("\nNovo salario sera de: R$ %.2lf\n", novo);
+            printf("Aumento: %.2lf\n", aumento);
+            printf("Aumento de: 5%%");
+        }   
 
     return 0;
 }
--- C/exercicios-curso/condicional/menor-de-tres.c
+++ C/exercicios-curso/condicional/menor-de-tres.c
@@ -14,13 +14,13 @@ int main () {
     printf("Digite o terceiro valor: ");
     scanf("%d", &c);
 
-    if (a < b && a < c){
-        menor = a;
-    } else if (b < c) {
-        menor = b;
-    } else {
-        menor = c;
-    }
+        if (a < b && a < c){
+            menor = a;
+        } else if (b < c) {
+            menor = b;
+        } else {
+            menor = c;
+        }
 
     printf("\n\nMenor = %d\n", menor);
     
