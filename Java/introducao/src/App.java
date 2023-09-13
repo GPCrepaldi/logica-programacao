cd948c2d479ee089f169780167bbb6c84d5de729
Gabriel Pedreiro Crepaldi <86982901+GPCrepaldi@users.noreply.github.com>
2023-09-12 21:18:05 -0300

    introducao

---
+++ Java/introducao/src/App.java
@@ -0,0 +1,21 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        int idade;
+        double salario, altura;
+        char genero;
+        String nome;
+
+        idade = 30;
+        salario = 5000.5;
+        altura = 1.72;
+        genero = 'F';
+        nome = "Maria Silva";
+
+        System.out.println("Idade = " + idade);
+        System.out.println("Salario = " + salario);
+        System.out.println("Altura = " + altura);
+        System.out.println("Genero = " + genero);
+        System.out.println("Nome = " + nome);
+    }
+}
