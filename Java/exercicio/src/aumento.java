b6e494c7e4b8d160f1e82547086363ae574b6887
Gabriel Pedreiro Crepaldi <86982901+GPCrepaldi@users.noreply.github.com>
2023-09-21 20:14:15 -0300

    aumento em Java

---
+++ Java/exercicio/src/aumento.java
@@ -0,0 +1,45 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class aumento {
+    public static void main(String[] args) throws Exception {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        double salario, novoSalario, aumento;
+
+        System.out.print("Digite o salario da pessoa: ");
+        salario = sc.nextDouble();
+
+        if (salario <= 1000){
+            novoSalario = salario * 1.2;
+            aumento = salario * 0.2;
+
+            System.out.println("Novo Salario: R$" + String.format("%.2f", novoSalario));
+            System.out.println("Aumento: R$" + String.format("%.2f", aumento));
+            System.out.println("Porcentagem: 20%");
+        } else if (salario < 3000) {
+            novoSalario = salario * 1.15;
+            aumento = salario * 0.15;
+
+            System.out.println("Novo Salario: R$" + String.format("%.2f", novoSalario));
+            System.out.println("Aumento: R$" + String.format("%.2f", aumento));
+            System.out.println("Porcentagem: 15%");
+        } else if (salario <= 8000) {
+            novoSalario = salario * 1.1;
+            aumento = salario * 0.1;
+
+            System.out.println("Novo Salario: R$" + String.format("%.2f", novoSalario));
+            System.out.println("Aumento: R$" + String.format("%.2f", aumento));
+            System.out.println("Porcentagem: 10%");
+        } else if (salario > 8000) {
+            novoSalario = salario * 1.05;
+            aumento = salario * 0.05;
+
+            System.out.println("Novo Salario: R$" + String.format("%.2f", novoSalario));
+            System.out.println("Aumento: R$" + String.format("%.2f", aumento));
+            System.out.println("Porcentagem: 5%");
+        }
+    }
+}
--- Java/introducao/src/dados.java
+++ Java/introducao/src/dados.java
@@ -5,36 +5,37 @@ public class dados {
     public static void main(String[] args) throws Exception {
     
         Locale.setDefault(Locale.US);
-        try (Scanner sc = new Scanner(System.in)) {
-            
-            String nome1, nome2;
-            double salario1, salario2;
-            int idade;
-            char sexo;
+        Scanner sc = new Scanner(System.in);
 
-            System.out.print("Digite o nome da primeira pessoa: ");
-            nome1 = sc.nextLine();
-            System.out.print("Digite o salario da primeira pessoa: ");
-            salario1 = sc.nextDouble();
+        String nome1, nome2;
+        double salario1, salario2;
+        int idade;
+        char sexo;
 
-            sc.nextLine();
-            System.out.print("Digite o nome da segunda pessoa: ");
-            nome2 = sc.nextLine();
-            System.out.print("Digite o salario da segunda pessoa: ");
-            salario2 = sc.nextDouble();
+        System.out.print("Digite o nome da primeira pessoa: ");
+        nome1 = sc.nextLine();
+        System.out.print("Digite o salario da primeira pessoa: ");
+        salario1 = sc.nextDouble();
 
-            System.out.print("Digite uma idade: ");
-            idade = sc.nextInt();
+        sc.nextLine();
+        System.out.print("Digite o nome da segunda pessoa: ");
+        nome2 = sc.nextLine();
+        System.out.print("Digite o salario da segunda pessoa: ");
+        salario2 = sc.nextDouble();
 
-            System.out.print("Digite o sexo: ");
-            sexo = sc.next().charAt(0);
+        System.out.print("Digite uma idade: ");
+        idade = sc.nextInt();
 
-            System.out.println("Nome1 = " + nome1);
-            System.out.println("Salario1 = " + String.format("%.2f", salario1));
-            System.out.println("Nome2 = " + nome2);
-            System.out.println("Salario2 = " + String.format("%.2f", salario2));
-            System.out.println("Idade = " + idade);
-            System.out.println("Sexo = " + sexo);
-        }
+        System.out.print("Digite o sexo: ");
+        sexo = sc.next().charAt(0);
+
+        System.out.println("Nome1 = " + nome1);
+        System.out.println("Salario1 = " + String.format("%.2f", salario1));
+        System.out.println("Nome2 = " + nome2);
+        System.out.println("Salario2 = " + String.format("%.2f", salario2));
+        System.out.println("Idade = " + idade);
+        System.out.println("Sexo = " + sexo);
+
+        sc.close();
     }
 }
